1d25af93386ba2f9e23f82a6005d406e6d0c6735
Hridoy2206 <ridoyahmed2206@gmail.com>
2023-07-21 04:00:23 +0600

    responsive problem solving"

--- src/pages/Home/Contact.jsx
+++ src/pages/Home/Contact.jsx
@@ -2,12 +2,12 @@ import CustomButton from "../../shared/CustomButton";
 
 const Contact = () => {
     return (
-        <div className=" lg:my-24 my-16 lg:mx-0 mx-8 mx-auto">
+        <div className=" lg:my-24 my-16  mx-auto">
             <span className="block text-center text-xl font-semibold">Contect</span>
             <h1 className="text-center lg:text-4xl text-3xl mb-12 font-extrabold text-[#1F1632]">
                 Let Us handle your <br /> project professionally
             </h1>
-            <div className="lg:w-1/2 mx-auto ">
+            <div className="lg:w-1/2 lg:mx-auto mx-5">
                 <div className="  grid lg:grid-cols-2 grid-cols-1 gap-5 ">
                     <input type="text" placeholder="Full name" className="bg-[#F1F3F6] p-3 rounded-md outline-none" />
                     <input type="text" placeholder="Last name" className="bg-[#F1F3F6] p-3 rounded-md outline-none" />
