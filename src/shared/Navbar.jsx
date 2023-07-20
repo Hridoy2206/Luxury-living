d5f60e267850447775a3ead8b7b558ececb35275
Hridoy2206 <ridoyahmed2206@gmail.com>
2023-07-21 00:09:05 +0600

    some changes

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
             <div className="text-4xl lg:hidden absolute right-9 top-5 cursor-pointer" onClick={() => setOpen(!open)}>
                 <ion-icon name={`${open ? "menu-outline" : "close-outline"}`}></ion-icon>
             </div>
-            <ul className={`lg:flex left-0 p-4 lg:p-0 gap-10 items-center absolute lg:z-auto bg-white w-full lg:static transition-all duration-500 ease-in ${!open ? "top-20" : "top-[-500px]"}`}>
+            <ul className={`lg:flex left-0 lg:pr-4 pr-64 p-4 lg:p-0 gap-10 items-center absolute lg:z-auto lg:bg-[#f6f6f6] bg-white lg:static transition-all duration-500 ease-in ${!open ? "top-20" : "top-[-500px]"}`}>
                 <li className="my-4"><Link to="/">Home</Link> </li>
                 <li className="my-4"><Link to="/about">About us</Link> </li>
                 <li className="my-4"><Link to="/project">Projects</Link> </li>
